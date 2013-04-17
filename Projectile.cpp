e46ed3c8fde75d13e14e1a7f6021183ad7e59052
Sofyan Saputra <ssaputra@nd.edu>
2013-04-17 10:26:25 -0400

    Projectile Collision Finished
    Projectiles will be erased when they hit a player, platform, or go off
    screen.

---
+++ Projectile.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "Projectile.h"
+#include <iostream>
+
+using namespace std;
+
+Projectile::Projectile(int x,int y, int h, int w, int t_vel, int t_type) : Entity (x, y, h, w) {
+	velX = t_vel;
+	type = t_type;
+	sprite = SDL_LoadBMP("Images/Sprites/Projectiles.bmp");
+
+}
+
+void Projectile::drawTo(SDL_Surface *surf) {
+	SDL_Rect src;
+	src.x = 0;
+	src.y = 0;
+	src.h = height;
+	src.w = width;
+
+	SDL_Rect dst;
+	dst.x = posX;
+	dst.y = posY;
+	dst.h = 0;
+	dst.w = 0;
+
+	SDL_BlitSurface(getSprite(), &src, surf, &dst);
+}
+
+void Projectile::onCollision(Entity *obj){
+	//How to deal with collisions with the player
+	if (obj->getID() == 2) {
+		//If bottom border is in a certain range of the platform
+		if ((posX > obj->getLeft() && posX < obj->getRight()) && (posY < obj->getBot() && posY > obj->getTop())) {
+			cout << "posX: " << posX << " obj left: " << obj->getLeft() << " obj Right: " << obj->getRight() <<endl;
+			cout << "posY: " << posY << " obj bot: " << obj->getBot() << " obj Top: " << obj->getTop() <<endl;
+			isGone = 1;		
+			cout << "onCollision isGone is " << isGone << endl;
+		}
+	}
+	if (obj->getID() == 3) {
+		//If bottom border is in a certain range of the platform
+		if ((posX > obj->getLeft() && posX < obj->getRight()) && (posY < obj->getBot() && posY > obj->getTop())) {
+			cout << "posX: " << posX << " obj left: " << obj->getLeft() << " obj Right: " << obj->getRight() <<endl;
+			cout << "posY: " << posY << " obj bot: " << obj->getBot() << " obj Top: " << obj->getTop() <<endl;
+			isGone = 1;		
+			cout << "onCollision isGone is " << isGone << endl;
+		}
+	}
+}
+
+void Projectile::move() {
+	posX += velX;
+}
+
+//Make sure to clear/free the projectiles
---
+++ SSB.cpp
@@ -0,0 +1,168 @@
+#include "stdafx.h"
+#include "SSB.h"
+#include <iostream>
+
+using namespace std;
+
+void SSB::execute() {
+	if (init()) {
+		//Main game loop
+		int running = 1;
+		while (running) {
+			running = events();
+			loop();
+			render();
+		}
+		cleanUp();
+	}
+	else cout << "SSB init failed" << endl;
+
+	SDL_Quit();
+}
+
+int SSB::init() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
+		cout << "SDL init failed" << endl;
+		return 0;
+	}
+
+
+	screen = SDL_SetVideoMode(800, 400, 32, SDL_HWSURFACE);
+	map = SDL_LoadBMP("Images/Maps/basic.bmp");
+
+	if (screen == NULL) {
+		cout << "Screen init failed" << endl;
+		return 0;
+	}
+
+	js_0 = SDL_JoystickOpen(0);
+	js_1 = SDL_JoystickOpen(1);
+	
+	if (!js_0) 
+		cout << "Joystick 1 failed to init" << endl;
+	if (!js_1) 
+		cout << "Joystick 2 failed to init" << endl;
+
+	if (!js_0 && !js_1) {
+		cout << "No controller was found" << endl;
+		return 0;
+	}
+
+	Platform *pf0 = new Platform(200, 350 , 400, 20, 1);
+	Platform *pf1 = new Platform(250, 250, 100, 20, 0);
+	Platform *pf2 = new Platform(450, 250, 100, 20, 0);
+	Platform *pf3 = new Platform(350, 150, 100, 20, 0);
+	Platform *pf4 = new Platform(50, 100, 100, 20, 0);
+	Platform *pf5 = new Platform(650, 100, 100, 20, 0);
+	
+	player0 = new Megaman(250, 50);
+	player1 = new Megaman(450, 50);
+	//Adds players to the events class
+	
+	queue.add(player0, player1);
+	
+	//Holds all entities created
+	entityList.push_back(player0);
+	entityList.push_back(player1);
+	entityList.push_back(pf0);
+	entityList.push_back(pf1);
+	entityList.push_back(pf2);
+	entityList.push_back(pf3);
+	entityList.push_back(pf4);
+	entityList.push_back(pf5);
+	return 1;
+}
+
+int SSB::events() {
+	return queue.resolve();
+}
+
+//Update everything
+void SSB::loop() {
+	//Variables for projectile collision
+	vector<Entity*> ptr0 = player0->getProjectileList();
+	vector<Entity*> ptr1 = player1->getProjectileList();
+
+	player0->move();
+	player0->offScreen();
+	player0->updateBorders();
+	player1->move();
+	player1->offScreen();
+	player1->updateBorders();
+
+	//Update Projectile Location Info
+	for (int j = 0; j < ptr0.size(); j++) {
+			ptr0[j]->updateBorders();
+	}
+	for (int j = 0; j < ptr1.size(); j++) {
+			ptr1[j]->updateBorders();
+	}
+
+	//Delete projectiles if they go off screen
+	for (int j = 0; j < ptr0.size(); j++) {
+		if(ptr0[j]->getRight() > 800 || ptr0[j]->getLeft() < 0){
+			player0->removeProj(j);
+		}
+	}
+	for (int j = 0; j < ptr1.size(); j++) {
+			if(ptr1[j]->getRight() > 800 || ptr1[j]->getLeft() < 0)
+				player1->removeProj(j);
+	}
+
+
+	//Check for collisions
+	for (int i = 0; i < entityList.size(); i++) {
+		for (int j = i + 1; j < entityList.size(); j++) {
+			if (entityList[i]->collides(entityList[j])) {
+				queue.addCollision(entityList[i], entityList[j]);
+			}
+		}
+
+		//Variables for projectile collision
+		ptr0 = player0->getProjectileList();
+		ptr1 = player1->getProjectileList();
+
+		//Player 1 Projectiles
+		for (int j = 0; j < ptr0.size(); j++) {
+			cout << entityList[i]->collides(ptr0[j]) << endl;
+			if (ptr0[j]->collides(entityList[i])){
+				/*
+				cout << "Left: " << ptr0[j]->getLeft() << endl;
+				cout << "Right: " << ptr0[j]->getRight() << endl;
+				cout << "Top: " << ptr0[j]->getTop() << endl;
+				cout << "Bot: " << ptr0[j]->getBot() << endl;
+				*/
+				queue.addCollision(entityList[i], ptr0[j]);
+			}
+		}
+
+		for (int k = 0; k < ptr0.size(); k++) {
+				if(ptr0[k]->getIsGone() == 1){
+					player0->removeProj(k);
+				}
+		}
+
+		//Player 2 Projectiles
+		for (int j  = 0; j < ptr1.size(); j++) {
+			if (entityList[i]->collides(ptr1[j]))
+				queue.addCollision(entityList[i], ptr1[j]);
+		}
+	}
+}
+
+void SSB::render() {
+	SDL_FillRect(screen, NULL, 0);
+	SDL_BlitSurface(map, NULL, screen, NULL);
+
+	for (int i = 0; i < entityList.size(); i++)
+		entityList[i]->drawTo(screen);
+
+	SDL_Flip(screen);
+}
+
+void SSB::cleanUp() {
+	delete screen, map, js_0, js_1;
+	for (int i = 0; i < entityList.size(); i++) {
+		delete entityList[i];
+	}
+}
