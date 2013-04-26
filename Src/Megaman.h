79b514b9dd231fd56a85b329fcb8bea0702eecee
Chan Lee <cglee.toqo@gmail.com>
2013-04-25 20:30:50 -0400

    Added todo list. Finalized merge.

--- Src/Megaman.h
+++ Src/Megaman.h
@@ -1,8 +1,8 @@
 #ifndef MEGAMAN_H
 #define MEGAMAN_H
 
-#include "SDL.h"
-#include "stdafx.h"
+//#include "SDL.h"
+//#include "stdafx.h"
 #include "BaseCharacter.h"
 #include "Entity.h"
 #include "Projectile.h"
